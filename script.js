b275917d5f4232e043eb415dcfe1606997fcf74e
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-06 19:19:39 +0100

    calcolo degli sconti e validazione

--- script.js
+++ script.js
@@ -21,16 +21,17 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 /*
 ANALISI
 1 - Prendere gli elementi dalla pagina.
-2 - Aggancio l'event listener al button che carica i dati.
-3 - Creare una variabile per il prezzo del biglietto per chilometro.
+2 - Creare una variabile per il prezzo del biglietto per chilometro.
+3 - Aggancio l'event listener al button che carica i dati.
 4 - Inserisco i chilometri che voglio percorrere e li salvo in una variabile.
 5 - Inserisco la mia età e la salvo in una variabile.
-6 - Calcolare il prezzo del biglietto in base ai chilometri (0.21 € al Km).
-7 - In base all'età verficare se l'utente ha diritto a uno sconto:
+6 - Controllo che il valore dei dati inseriti sia corretto.
+7 - Calcolare il prezzo del biglietto in base ai chilometri (0.21 € al Km).
+8 - In base all'età verficare se l'utente ha diritto a uno sconto:
     -- l'utente ha un'età compresa tra i 19 e i 64 anni: nessuno sconto.
     -- l'utente ha un'età minore o uguale ai 18 anni: calcola lo sconto del 20%.
     -- l'utente ha un'età maggiore o uguale a 65 anni: calcola lo sconto del 40%.
-8 - Stampare il prezzo totale del viaggio.
+9 - Stampare in pagina un form con i dati inseriti e il prezzo da pagare e stlilizzarlo.
 */
 
 // 1 - Prendo gli elementi dalla pagina
@@ -40,31 +41,40 @@ const upLoad = document.getElementById("upload");
 
 console.log(distance, age, upLoad);
 
+// 2 - Creo una variabile per il prezzo del biglietto per chilometro.
+const unitPrice = 0.21;
 
-// 2 - Aggancio l'event listener al button che carica i dati.
+// 3 - Aggancio l'event listener al button che carica i dati.
 upLoad.addEventListener('click' , function(){
-
-// 3 - Creo una variabile per il prezzo del biglietto per chilometro.
-    const unitPrice = 0.21;
-
-// 4e5 - Inserisco i dati richiesti e li salvo in una variabile. 
-    const textDistance = distance.value;
-    const textAge = age.value;
-    console.log(textDistance, textAge);
-
-// 6 - Calcolo il prezzo del biglietto in base ai chilometri da percorrere.
-    let grossPrice = (unitPrice * textDistance).toFixed(2);    
     
-// 7 - in base all'età verificare se l'utente ha diritto a uno sconto.   
-    if(textAge > 18 && textAge < 65) {
+// 4e5 - Inserisco i dati richiesti e li salvo in una variabile. 
+const textDistance = distance.value;
+const textAge = age.value;
+console.log(textDistance, textAge);
+
+// 6 - Controllo che il valore dei dati inseriti sia corretto.
+    if (isNaN(textDistance) || isNaN(textAge) || textDistance <= 0 || textAge <= 0) {
+       alert("inserire solo valori numerici interi e maggiori di 0")
+       location.reload();
+
+    } else {
+
+    // 7 - Calcolo il prezzo del biglietto in base ai chilometri da percorrere.
+    const grossPrice = (unitPrice * textDistance).toFixed(2);    
+    
+    // 8 - in base all'età verificare se l'utente ha diritto a uno sconto.   
+    if (textAge > 18 && textAge < 65) {
        console.log(`Il prezzo da pagare è £ ${grossPrice}`)
     
     } else if(textAge < 19){
        const smallDiscount = ((grossPrice / 100) * 80).toFixed(2);
        console.log(`Il prezzo per i minorenni è di ${smallDiscount}`)
+
     } else {
        const bigDiscount = ((grossPrice / 100) * 60).toFixed(2);
        console.log(`Il prezzo per gli over 65 è di ${bigDiscount}`)
+    }
+
     }
 
 });
