bb837e80d7aa911915cc4ffc2609141b956a0284
AlessandroLembo <alessandrolembo2@gmail.com>
2022-12-06 15:36:52 +0100

    markup

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 console.log(" JS OK ");
+
+/* 
+TRACCIA
+
+Descrizione:
+Scrivere un programma che chieda all’utente:
+- Il numero di chilometri da percorrere
+- Età del passeggero
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+MILESTONE 1:
+Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
+MILESTONE 2:
+Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
+Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+*/
+
+/*
+ANALISI
+1 - Prendere gli elementi dalla pagina.
+2 - Creare una variabile per il prezzo del biglietto per chilometro. 
+3 - Chiedere all'utente quanti chilometri vuole percorrere e salvare la risposta nella variabile. 
+4 - Chiedere all'utente la sua età e salvare la risposta nella variabile.
+5 - Calcolare il prezzo del biglietto in base ai chilometri (0.21 € al Km).
+6 - In base all'età verficare se l'utente ha diritto a uno sconto:
+    -- l'utente ha un'età compresa tra i 19 e i 64 anni: nessuno sconto.
+    -- l'utente ha un'età minore o uguale ai 18 anni: calcola lo sconto del 20%.
+    -- l'utente ha un'età maggiore o uguale a 65 anni: calcola lo sconto del 40%.
+7 - Stampare il prezzo totale del viaggio.
+*/
+
+// 1 - Prendo gli elementi dalla pagina
+
